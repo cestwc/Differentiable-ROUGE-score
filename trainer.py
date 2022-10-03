3f76108e0377ac29fa0b63c32112edbb83d8f85e
cestwc <80936226+cestwc@users.noreply.github.com>
2022-10-03 14:49:58 +0800

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -1,6 +1,8 @@
 from torch import nn
 from transformers import Trainer
 
+from losses import ROUGELoss
+
 class RougeTrainer(Trainer):
 	def compute_loss(self, model, inputs, return_outputs=False):
 		labels = inputs.get("labels")
