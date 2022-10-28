3cf798ee5ae1cc367c4b117eed2616f1aec5e619
Ubuntu <azureuser@vm1.1xhxntq54jfuzh5tmwqw1we3lb.ix.internal.cloudapp.net>
2022-10-28 06:26:54 +0000

    train

--- losses.py
+++ losses.py
@@ -16,16 +16,16 @@ class ROUGELoss(torch.nn.Module):
 		self.n = n
 		self.max_length = max_length
 	def forward(self, logits, labels):
-		logits = logits[:, :self.max_length, :]
-		labels = labels[:, :self.max_length]
-		labels_attend = (labels != 1).float()
+		# logits = logits[:, :self.max_length, :]
+		# labels = labels[:, :self.max_length]
+		# labels_attend = (labels != 1).float()
 		
-		attend = torch.bmm(labels_attend.unsqueeze(2), labels_attend.unsqueeze(1)).unsqueeze(1)
+		# attend = torch.bmm(labels_attend.unsqueeze(2), labels_attend.unsqueeze(1)).unsqueeze(1)
 		# overlap = F.embedding(labels.view(-1), F.softmax(logits.view(-1, logits.shape[-1]), dim=-1).T)
-		overlap =  attend * torch.stack([F.embedding(b, a.T) for a, b in zip(discrete_softmax(logits, -1), labels)]).unsqueeze(1)
+		overlap =  torch.stack([F.embedding(b, a.T) for a, b in zip(discrete_softmax(logits, -1), labels)]).unsqueeze(1)
 		numerators = - sudoku(overlap).sum((-2, -1))
-		# denominators = torch.tensor(overlap.shape[-2:]) - self.n + 1
-		denominators = labels_attend.sum(1)
+		denominators = torch.tensor(overlap.shape[-2:]) - self.n + 1
+		# denominators = labels_attend.sum(1)
 		if self.reduction == 'mean':
 			return (numerators / denominators).mean()
 		else:
