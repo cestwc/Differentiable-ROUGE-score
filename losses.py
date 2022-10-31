9ab489f1b262abf5564ac87364eee5723461b4ab
cestwc <80936226+cestwc@users.noreply.github.com>
2022-11-01 01:33:18 +0800

    Update losses.py

--- losses.py
+++ losses.py
@@ -27,6 +27,6 @@ class ROUGELoss(torch.nn.Module):
 		denominators = sum(overlap.shape[-2:]) - self.n + 1
 		# denominators = labels_attend.sum(1)
 		if self.reduction == 'mean':
-			return 1 - (numerators / denominators).mean()
+			return 1 + (numerators / denominators).mean()
 		else:
 			return overlap
