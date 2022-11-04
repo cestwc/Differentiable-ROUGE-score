661ee59644111022b09b9953ff7d6e1072434ac8
cestwc <80936226+cestwc@users.noreply.github.com>
2022-11-04 19:51:01 +0800

    Update conv_core.py

--- conv_core.py
+++ conv_core.py
@@ -45,7 +45,7 @@ def sudoku(x):
 	for i in range(128):
 		x = F.conv2d(F.pad(x, (1, 0, 1, 0)), w12.to(x.device))
 
-	return F.leaky_relu(F.leaky_relu(1 - abs(x[:, 1:2] - x[:, 2:3]))  + x[:, 0:1] - 1)
+	return F.relu(F.relu(1 - abs(x[:, 1:2] - x[:, 2:3]))  + x[:, 0:1] - 1)
 
 def diagonal2(x):
 	x = F.conv2d(F.pad(x, (1, 0, 1, 0)),
