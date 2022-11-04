7d584de3980ffb93fe72693fb2e0b961f38b81d4
cestwc <80936226+cestwc@users.noreply.github.com>
2022-11-04 19:50:24 +0800

    Update losses.py

--- losses.py
+++ losses.py
@@ -19,15 +19,27 @@ class ROUGELoss(torch.nn.Module):
 		# logits = logits[:, :self.max_length, :]
 		# labels = labels[:, :self.max_length]
 		# labels_attend = (labels != 1).float()
-		
+
 		# attend = torch.bmm(labels_attend.unsqueeze(2), labels_attend.unsqueeze(1)).unsqueeze(1)
 		# overlap = F.embedding(labels.view(-1), F.softmax(logits.view(-1, logits.shape[-1]), dim=-1).T)
-		overlap =  torch.stack([F.embedding(b, a.T) for a, b in zip(F.softmax(logits, -1), labels)]).unsqueeze(1)
-		return 1 - overlap.mean()
-		# numerators = - sudoku(overlap).sum((-2, -1)) * 2
-		# denominators = sum(overlap.shape[-2:]) - self.n + 1
-		# # denominators = labels_attend.sum(1)
-		# if self.reduction == 'mean':
-		# 	return 1 + (numerators / denominators).mean()
-		# else:
-		# 	return overlap
+		overlap =  torch.stack([F.embedding(b, a.T) for a, b in zip(discrete_softmax(logits, -1), labels)]).unsqueeze(1)
+		overlap = sudoku(overlap).long().float().detach()
+		for k in range(len(overlap)):
+			h = overlap[k].sum(-2).squeeze()
+			v = overlap[k].sum(-1).squeeze()
+			for kh in range(len(h)):
+			for kv in range(len(v)):
+				if h[kh] == 0 and v[kv] == 0:
+				overlap[k, 0, kv, kh] = 0.5
+		overlap = torch.clamp(overlap, 1e-1, 1)
+
+		probs =  torch.stack([F.embedding(b, a.T) for a, b in zip(F.softmax(logits, -1), labels)]).unsqueeze(1)
+		numerators = - (overlap * probs).sum((-2, -1)) * 2
+		denominators = sum(overlap.shape[-2:]) - self.n + 1
+
+
+		# denominators = labels_attend.sum(1)
+		if self.reduction == 'mean':
+			return 1 + (numerators / denominators).mean()
+		else:
+			return overlap
