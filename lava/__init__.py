9fac233390ba2a642fa6156efac5474ffc3e418b
cestwc <ruihan@SJgroup.uxb2jmgf02de3dtvd1ysb0bckh.ix.internal.cloudapp.net>
2023-02-20 10:03:42 +0000

    create a pretty hard-coded small model with shallower decoder

--- lava/__init__.py
+++ lava/__init__.py
@@ -1,3 +1,4 @@
 _import_structure = {"configuration_lava": ["LavaConfig"]}
 
 from .modeling_lava import LavaModel
+from .modeling_lava_small import LavaModelSmall
---
+++ lava/modeling_lava_small.py
@@ -0,0 +1,42 @@
+from typing import Optional
+
+import torch
+from torch import nn
+from torch.nn import CrossEntropyLoss
+
+from transformers import PreTrainedModel, AutoModelForMaskedLM, AutoModelForQuestionAnswering, BartConfig
+from transformers.modeling_outputs import BaseModelOutput, Seq2SeqLMOutput, MaskedLMOutput
+from .configuration_lava import LavaConfig
+from .modeling_lava import LavaModel
+
+class LavaModelSmall(LavaModel):
+
+    def __init__(self, config):
+        super().__init__(config)
+
+    @classmethod
+    def from_lava_pretrained(
+        cls,
+        encoder_pretrained_model_name_or_path: str = None,
+        decoder_pretrained_model_name_or_path: str = None,
+        *model_args,
+        **kwargs,
+    ) -> PreTrainedModel:
+
+        encoder = AutoModelForMaskedLM.from_pretrained(encoder_pretrained_model_name_or_path)
+        decoder_ = AutoModelForQuestionAnswering.from_pretrained(decoder_pretrained_model_name_or_path)
+        decoder_state_dict = decoder_.state_dict()
+        decoder = AutoModelForQuestionAnswering.from_config(
+            BartConfig(encoder_layers = 1, decoder_layers = 1, d_model = 768, decoder_ffn_dim = 3072, encoder_ffn_dim = 3072)
+            )
+
+        decoder.load_state_dict({k:decoder_state_dict[k] for k in decoder.state_dict()})
+        
+        config = LavaConfig.from_encoder_decoder_configs(encoder.config, decoder.config, **kwargs)
+        inst = cls(config)
+        inst.encoder = encoder
+        inst.decoder = decoder
+
+        inst.decoder.qa_outputs.bias.requires_grad = False
+
+        return inst
